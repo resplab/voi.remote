8676fdda2eb8c7f79e16a88fef45b65df1bf5112
William Xu <yihangxu001@hotmail.com>
2021-08-27 11:56:34 -0700

    initial package setup

---
+++ R/voi_remote.R
@@ -0,0 +1,117 @@
+#' Title
+#'
+#' @param outputs
+#' @param nsim
+#'
+#' @return
+#' @export
+#'
+#' @examples
+evpi.remote <- function(outputs, nsim) {
+  model_input <- list(outputs=outputs, nsim=nsim, func='evpi')
+  res <- peermodels::model_run(model_name='voi', model_input, api_key='aaHYJJb4hcrmBYY3')
+  return(res)
+}
+
+
+#' Title
+#'
+#' @param outputs
+#' @param inputs
+#' @param pars
+#' @param se
+#' @param B
+#' @param nsim
+#' @param verbose
+#' @param method
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+evppi.remote <- function(outputs, inputs, pars, se, B, nsim, verbose, method, ...) {
+  model_input <- list(outputs=outputs, inputs=inputs, pars=pars, se=se, B=B, nsim=nsim, verbose=verbose, func='evppi', method=method, etc=...)
+  res <- peermodels::model_run(model_name='voi', model_input, api_key='aaHYJJb4hcrmBYY3')
+  return(res)
+}
+
+
+#' Title
+#'
+#' @param model_fn
+#' @param par_fn
+#' @param pars
+#' @param nouter
+#' @param ninner
+#' @param wtp
+#' @param mfargs
+#' @param verbose
+#'
+#' @return
+#' @export
+#'
+#' @examples
+evppi_mc.remote <- function(model_fn, par_fn, pars, nouter, ninner, wtp, mfargs, verbose) {
+  model_input <- list(model_fn=model_fn, par_fn=par_fn, pars=pars, nouter=nouter, ninner=ninner, wtp=wtp, mfargs=mfargs, verbose=verbose, func='evppi_mc')
+  res <- peermodels::model_run(model_name='voi', model_input, api_key='aaHYJJb4hcrmBYY3')
+  return(res)
+}
+
+#' Title
+#'
+#' @param outputs
+#' @param inputs
+#' @param study
+#' @param datagen_fn
+#' @param pars
+#' @param n
+#' @param likelihood
+#' @param analysis_model
+#' @param analysis_options
+#' @param decision_model
+#' @param Q
+#' @param npreg_method
+#' @param nsim
+#' @param verbose
+#' @param method
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+evsi.remote <- function(outputs, inputs, study, datagen_fn, pars, n, likelihood, analysis_model, analysis_options, decision_model, Q, npreg_method, nsim, verbose, method, ...) {
+  model_input <- list(outputs=outputs, inputs=inputs, study=study, datagen_fn=datagen_fn, pars=pars, n=n, likelihood=likelihood, analysis_model=analysis_model, analysis_options=analysis_options, decision_model=decision_model, Q=Q, npreg_method=npreg_method, nsim=nsim, verbose=verbose, func='evsi', method=method, etc=...)
+  res <- peermodels::model_run(model_name='voi', model_input, api_key='aaHYJJb4hcrmBYY3')
+  return(res)
+}
+
+#' Title
+#'
+#' @param outputs
+#' @param inputs
+#' @param study
+#' @param datagen_fn
+#' @param pars
+#' @param n
+#' @param likelihood
+#' @param analysis_model
+#' @param analysis_options
+#' @param decision_model
+#' @param Q
+#' @param npreg_method
+#' @param nsim
+#' @param verbose
+#' @param method
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+evsivar.remote <- function(outputs, inputs, study, datagen_fn, pars, n, likelihood, analysis_model, analysis_options, decision_model, Q, npreg_method, nsim, verbose, method, ...) {
+  model_input <- list(outputs=outputs, inputs=inputs, study=study, datagen_fn=datagen_fn, pars=pars, n=n, likelihood=likelihood, analysis_model=analysis_model, analysis_options=analysis_options, decision_model=decision_model, Q=Q, npreg_method=npreg_method, nsim=nsim, verbose=verbose, func='evsivar', method=method, etc=...)
+  res <- peermodels::model_run(model_name='voi', model_input, api_key='aaHYJJb4hcrmBYY3')
+  return(res)
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# voi.remote
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+The goal of voi.remote is to …
+
+## Installation
+
+You can install the released version of voi.remote from
+[CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("voi.remote")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(voi.remote)
+## basic example code
+```
